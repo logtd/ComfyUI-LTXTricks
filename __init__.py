43c7f490dc7e4ac219920b2a1419478a2ad12a42
logtd <logoftd@gmail.com>
2024-11-23 22:58:35 -0700

    add nodes

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from .nodes.modify_ltx_model_node import ModifyLTXModelNode
 from .nodes.latent_guide_node import AddLatentGuideNode
 from .nodes.ltx_inverse_model_pred_nodes import LTXForwardModelSamplingPredNode, LTXReverseModelSamplingPredNode
 from .nodes.rectified_sampler_nodes import LTXRFForwardODESamplerNode, LTXRFReverseODESamplerNode
+from .nodes.attn_bank_nodes import LTXPrepareAttnInjectionsNode, LTXAttentionBankNode, LTXAttentioOverrideNode
 
 NODE_CLASS_MAPPINGS = {
     'ModifyLTXModel': ModifyLTXModelNode,
@@ -10,6 +11,9 @@ NODE_CLASS_MAPPINGS = {
     'LTXReverseModelSamplingPred': LTXReverseModelSamplingPredNode,
     'LTXRFForwardODESampler': LTXRFForwardODESamplerNode,
     'LTXRFReverseODESampler': LTXRFReverseODESamplerNode,
+    'LTXAttentionBank': LTXAttentionBankNode,
+    'LTXPrepareAttnInjections': LTXPrepareAttnInjectionsNode,
+    'LTXAttentioOverride': LTXAttentioOverrideNode,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -18,5 +22,8 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     'LTXForwardModelSamplingPred': 'LTX Forward Model Pred',
     'LTXReverseModelSamplingPred': 'LTX Reverse Model Pred',
     'LTXRFForwardODESampler': 'LTX Rf-Inv Forward Sampler',
-    'LTXRFReverseODESampler': 'LTX Rf-Inv Reverse Sampler'
+    'LTXRFReverseODESampler': 'LTX Rf-Inv Reverse Sampler',
+    'LTXAttentionBank': 'LTX Attention Bank',
+    'LTXPrepareAttnInjections': 'LTX Prepare Attn Injection',
+    'LTXAttentioOverride': 'LTX Attn Block Override',
 }
